0e3d27c61ef4aabfcb46d3ba16b41cf572fa53fb
EunJi <hej0102@naver.com>
2022-03-19 23:14:05 +0900

    Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+let month = 3
+let toDo = "Travel"
+
+var toDoArray = ["Travel", "Work", "Call"]
+var evenNumber = [2, 4, 6, 8]
+
+toDoArray[0] = "Play"
+toDoArray[1]
+toDoArray[2]
+
+evenNumber.append(10)
+evenNumber.insert(12, at:0)
+
+evenNumber.remove(at: 0)
+print(evenNumber)
+
+for toDo in toDoArray {
+    print(toDo)
+}
