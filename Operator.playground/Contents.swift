3c7d7d53ddba7b677c1c7fe7583e0ac01d5839b7
EunJi <>
2022-03-22 00:12:10 +0900

    Operator

---
+++ Operator.playground/Contents.swift
@@ -0,0 +1,29 @@
+// 산술 연산자
+var weight = 65
+weight = weight + 10
+weight = weight - 10
+weight = weight * 10
+weight = weight / 10
+weight = weight % 10
+
+weight = 65
+weight += 10
+weight -= 10
+weight *= 10
+weight /= 10
+weight %= 10
+
+
+// 비교 연산자
+// <, >, ==, !=
+let age = 25
+let isAdult = age >= 20
+let 중1 = age == 14
+
+
+// 논리 연산자
+// &&, ||, !
+// 조건식1 && 조건식2 => 두 가지 조건식 모두 true일 때 결과 true
+// 조건식1 || 조건식2 => 두 가지 조건식 중 하나 이상 true일 때 결과 true
+// ! => true, flase 바꿈
+let 초등학생 = age >= 8 && age <= 13
