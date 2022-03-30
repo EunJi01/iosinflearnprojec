5af2df2a6ac01e4ed5da772819d441a6d052af28
EunJi <hej0102@naver.com>
2022-03-30 17:36:36 +0900

    Flashlight

---
+++ Flashlight/Flashlight/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  SceneDelegate.swift
+//  Flashlight
+//
+//  Created by 황은지 on 2022/03/07.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var flashImageView: UIImageView!
+    
+    var isOn = false
+    var soundPlayer: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        prepareSound()
+    }
+
+    func prepareSound() {
+        let path = Bundle.main.path(forResource: "switch.wav", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.prepareToPlay()
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
+    @IBAction func switchTapped(_ sender: Any) {
+        isOn = !isOn
+        
+        soundPlayer?.play()
+        toggleTorch(on: isOn)
+        
+        flashImageView.image = isOn ? #imageLiteral(resourceName: "onBG") : #imageLiteral(resourceName: "offBG")
+        switchButton.setImage(isOn ? #imageLiteral(resourceName: "onSwitch") : #imageLiteral(resourceName: "offSwitch"), for: .normal)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
